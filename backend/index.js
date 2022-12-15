333b597f830c4c894406d5cf06f962147a2be847
LamNguyen <lamnt.8x@gmail.com>
2022-12-15 23:18:03 +0700

    Initial project

---
+++ backend/index.js
@@ -0,0 +1,15 @@
+import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+dotenv.config()
+
+import express from "express"
+
+const app = express()
+const port = process.env.PORT || 5000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
