e83d7792fbb04bfc1825a63be71279c34978966c
LamNguyen <lamnt.8x@gmail.com>
2022-12-16 17:20:21 +0700

    Update backend

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,26 @@
-import * as dotenv from 'dotenv' // see https://github.com/motdotla/dotenv#how-do-i-use-dotenv-with-import
+import * as dotenv from 'dotenv';
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors'
+
+import posts from './routers/posts.js';
+
 dotenv.config()
 
-import express from "express"
+const app = express();
+const port = process.env.PORT || 3000;
 
-const app = express()
-const port = process.env.PORT || 5000
+app.use('/static', express.static('public'))
+app.use(bodyParser.json({limit: '30mb'}))
+app.use(bodyParser.urlencoded({ extended: true, limit: '30mb'}))
+app.use(cors())
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.use('/posts', posts)
+
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Mern app blog listening on port ${port}`)
 })
